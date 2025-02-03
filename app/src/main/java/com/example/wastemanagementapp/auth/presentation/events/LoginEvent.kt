42ac5b866a51bd96dac99be2b44217269a237fb2
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-03 19:45:53 +0530

    All the events related to login screen.

---
+++ app/src/main/java/com/example/wastemanagementapp/auth/presentation/events/LoginEvent.kt
@@ -0,0 +1,12 @@
+package com.example.wastemanagementapp.auth.presentation.events
+
+sealed interface LoginEvent {
+
+    data object OnSignInClick : LoginEvent
+
+    data object OnSignUpClick : LoginEvent
+
+    data class OnEmailChange(val email: String) : LoginEvent
+
+    data class OnPasswordChange(val password: String) : LoginEvent
+}
