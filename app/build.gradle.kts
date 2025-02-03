3919269a46c809d67897061c880c5b5d36fbd4d8
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-03 19:49:41 +0530

    Additional dependency for google sign in.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
