4c93fe33668f634ab63be56ed32294e6b346c385
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-05 10:18:53 +0530

    Implemented normal sign in and sign up screen navigation. Observing auth state.

--- app/src/main/java/com/example/wastemanagementapp/auth/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/wastemanagementapp/auth/presentation/viewmodel/LoginViewModel.kt
@@ -9,13 +9,22 @@ import androidx.lifecycle.viewModelScope
 import com.example.wastemanagementapp.auth.domain.LoginRepository
 import com.example.wastemanagementapp.auth.presentation.events.LoginEvent
 import com.example.wastemanagementapp.core.domain.UserProfile
+import com.example.wastemanagementapp.core.util.NavigationEvent
+import com.example.wastemanagementapp.core.util.Screen
 import com.example.wastemanagementapp.core.util.SnackBarController
 import com.example.wastemanagementapp.core.util.SnackBarEvent
+import com.google.firebase.Firebase
 import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -28,13 +37,17 @@ class LoginViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val _userProfile = MutableStateFlow(UserProfile())
-    val userProfile = _userProfile.asStateFlow()
+
+    val authState : StateFlow<FirebaseUser?> get() = loginRepository.authState
 
     var email by mutableStateOf("")
         private set
 
     var password by mutableStateOf("")
         private set
+
+    private val _navigationEvent = Channel<NavigationEvent>()
+    val navigationEvent = _navigationEvent.receiveAsFlow()
 
     fun saveGoogleUser(authResult: Result<AuthResult>) {
         authResult.fold(
@@ -50,6 +63,12 @@ class LoginViewModel @Inject constructor(
 
                 viewModelScope.launch {
                     saveUserProfile(_userProfile.value)
+                    SnackBarController.sendEvent(
+                        event = SnackBarEvent(
+                            message = "Login successful"
+                        )
+                    )
+                    sendEvent(NavigationEvent.Navigate(Screen.HomeScreen))
                 }
             },
             onFailure = {
@@ -75,13 +94,64 @@ class LoginViewModel @Inject constructor(
     fun onEvent(event: LoginEvent) {
         when (event) {
             is LoginEvent.OnEmailChange -> {
-                email += event.email
+                email = event.email
             }
+
             is LoginEvent.OnPasswordChange -> {
-                password += event.password
+                password = event.password
             }
-            LoginEvent.OnSignInClick -> TODO()
-            LoginEvent.OnSignUpClick -> TODO()
+
+            LoginEvent.OnSignInClick -> {
+                viewModelScope.launch {
+                    if (email.isNotEmpty() && password.isNotEmpty()) {
+                        emailVerifiedOrNot(email).let { verified ->
+                            if (verified == true) {
+                                loginUserWithEmailAndPassword(email, password)
+
+                                SnackBarController.sendEvent(
+                                    event = SnackBarEvent(
+                                        message = "Login successful"
+                                    )
+                                )
+                            } else {
+                                SnackBarController.sendEvent(
+                                    event = SnackBarEvent(
+                                        message = "Email is not verified"
+                                    )
+                                )
+                            }
+                        }
+
+                        sendEvent(NavigationEvent.Navigate(Screen.HomeScreen))
+                    } else {
+                        SnackBarController.sendEvent(
+                            event = SnackBarEvent(
+                                message = "Fill all the fields"
+                            )
+                        )
+                    }
+                }
+            }
+
+            LoginEvent.OnSignUpClick -> {
+                sendEvent(NavigationEvent.Navigate(Screen.SignUpScreen))
+            }
         }
+    }
+
+    private fun sendEvent(event: NavigationEvent) {
+        viewModelScope.launch {
+            _navigationEvent.send(event)
+        }
+    }
+
+    private fun loginUserWithEmailAndPassword(email: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            loginRepository.loginUserWithEmailAndPassword(email, password)
+        }
+    }
+
+    private suspend fun emailVerifiedOrNot(email: String): Boolean? {
+        return loginRepository.emailVerifiedOrNot(email)
     }
 }
