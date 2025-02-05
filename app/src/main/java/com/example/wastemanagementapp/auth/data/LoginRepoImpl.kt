927b3607f9c60d6619d3f5d2f08b7b9c1e4876a8
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-05 10:23:05 +0530

    Email verification, login using email and password and auth state abstraction and implementation.

--- app/src/main/java/com/example/wastemanagementapp/auth/data/LoginRepoImpl.kt
+++ app/src/main/java/com/example/wastemanagementapp/auth/data/LoginRepoImpl.kt
@@ -3,11 +3,27 @@ package com.example.wastemanagementapp.auth.data
 import android.util.Log
 import com.example.wastemanagementapp.auth.domain.LoginRepository
 import com.example.wastemanagementapp.core.domain.UserProfile
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class LoginRepoImpl(
-    private val firebaseFireStore: FirebaseFirestore
+    private val firebaseFireStore: FirebaseFirestore,
+    private val firebaseAuth: FirebaseAuth
 ) : LoginRepository {
+
+    private val _authState = MutableStateFlow<FirebaseUser?>(null)
+    override val authState: StateFlow<FirebaseUser?>
+        get() = _authState
+
+    init {
+        firebaseAuth.addAuthStateListener { auth ->
+            _authState.value = auth.currentUser
+        }
+    }
+
     override suspend fun saveGoogleUserProfile(userProfile: UserProfile) {
         val userRef = firebaseFireStore.collection("user").document(userProfile.uuid)
 
@@ -23,6 +39,20 @@ class LoginRepoImpl(
                     "save",
                     "Error writing to document: $exception"
                 )
+            }
+    }
+
+    override suspend fun emailVerifiedOrNot(email: String): Boolean? {
+        return firebaseAuth.currentUser?.isEmailVerified
+    }
+
+    override suspend fun loginUserWithEmailAndPassword(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                Log.i("login", "loginUserWithEmailAndPassword: login successful")
+            }
+            .addOnFailureListener {
+                Log.i("login", "loginUserWithEmailAndPassword: login unsuccessful $it")
             }
     }
 }
--- app/src/main/java/com/example/wastemanagementapp/auth/domain/LoginRepository.kt
+++ app/src/main/java/com/example/wastemanagementapp/auth/domain/LoginRepository.kt
@@ -1,7 +1,14 @@
 package com.example.wastemanagementapp.auth.domain
 
 import com.example.wastemanagementapp.core.domain.UserProfile
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.StateFlow
 
 interface LoginRepository {
+    val authState : StateFlow<FirebaseUser?>
     suspend fun saveGoogleUserProfile(userProfile: UserProfile)
+
+    suspend fun emailVerifiedOrNot(email: String) : Boolean?
+
+    suspend fun loginUserWithEmailAndPassword(email: String, password: String)
 }
