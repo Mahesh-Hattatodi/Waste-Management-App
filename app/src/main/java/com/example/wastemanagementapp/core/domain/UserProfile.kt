e1c2e6f7527066935f2a4402b849d2bfb0b03804
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-06 12:38:49 +0530

    Made uid as nullable

--- app/src/main/java/com/example/wastemanagementapp/core/domain/UserProfile.kt
+++ app/src/main/java/com/example/wastemanagementapp/core/domain/UserProfile.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.Stable
 
 @Stable
 data class UserProfile(
-    val uuid: String = "",
+    val uuid: String? = "",
     val displayName: String? = "Loading",
     val email: String? = "Loading",
     val photoUrl: String? = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YreOWfDX3kK-QLAbAL4ufCPc84ol2MA8Xg&s"
