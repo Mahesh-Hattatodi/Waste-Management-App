3429d9cfcd32e30e226663e8778027c4f9317863
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-06 12:51:49 +0530

    Provided use case and repository objects

--- app/src/main/java/com/example/wastemanagementapp/auth/di/AuthModule.kt
+++ app/src/main/java/com/example/wastemanagementapp/auth/di/AuthModule.kt
@@ -1,7 +1,12 @@
 package com.example.wastemanagementapp.auth.di
 
 import com.example.wastemanagementapp.auth.data.LoginRepoImpl
+import com.example.wastemanagementapp.auth.data.SignUpRepoImpl
 import com.example.wastemanagementapp.auth.domain.LoginRepository
+import com.example.wastemanagementapp.auth.domain.SignUpRepository
+import com.example.wastemanagementapp.auth.domain.use_cases.ValidateConfirmPassword
+import com.example.wastemanagementapp.auth.domain.use_cases.ValidateEmail
+import com.example.wastemanagementapp.auth.domain.use_cases.ValidatePassword
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
@@ -21,5 +26,32 @@ class AuthModule {
         firebaseAuth: FirebaseAuth
     ) : LoginRepository {
         return LoginRepoImpl(firebaseFireStore, firebaseAuth)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSignUpRepository(
+        firebaseAuth: FirebaseAuth,
+        firebaseFireStore: FirebaseFirestore
+    ) : SignUpRepository {
+        return SignUpRepoImpl(firebaseAuth, firebaseFireStore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideValidateEmailUseCase() : ValidateEmail {
+        return ValidateEmail()
+    }
+
+    @Provides
+    @Singleton
+    fun provideValidatePasswordUseCase() : ValidatePassword {
+        return ValidatePassword()
+    }
+
+    @Provides
+    @Singleton
+    fun provideValidateConfirmPasswordUseCase() : ValidateConfirmPassword {
+        return ValidateConfirmPassword()
     }
 }
