0e6f63946bb31d6413fd814b11c6e83ffc661275
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-07 09:45:03 +0530

    Home screen navigation placeholder.

---
+++ app/src/main/java/com/example/wastemanagementapp/home/presentation/HomeScreenContainer.kt
@@ -0,0 +1,9 @@
+package com.example.wastemanagementapp.home.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreenContainer() {
+    Text(text = "Home screen")
+}
