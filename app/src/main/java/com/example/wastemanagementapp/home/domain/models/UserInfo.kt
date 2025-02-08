2949c2ab94be43ca71a13a36d369dbaffd4aa2e6
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-08 22:04:05 +0530

    User information holder for home screen.

---
+++ app/src/main/java/com/example/wastemanagementapp/home/domain/models/UserInfo.kt
@@ -0,0 +1,9 @@
+package com.example.wastemanagementapp.home.domain.models
+
+data class UserInfo(
+    val ranking: String = "",
+    val photoUrl: String = "",
+    val name: String = "",
+    val email: String = "",
+    val uid: String = ""
+)
