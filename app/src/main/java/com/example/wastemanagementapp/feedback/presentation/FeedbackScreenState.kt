79c902f5de9f42dbceb754f1b65bf02a313f54a3
Mahesh-Hattatodi <maheshh1323@gmail.com>
2025-02-11 21:08:14 +0530

    Feedback screen state

---
+++ app/src/main/java/com/example/wastemanagementapp/feedback/presentation/FeedbackScreenState.kt
@@ -0,0 +1,8 @@
+package com.example.wastemanagementapp.feedback.presentation
+
+data class FeedbackScreenState(
+    val emoji: String = "",
+    val selectedTopic: String = "Select topic",
+    val feedback: String = "",
+    val isExpanded: Boolean = false
+)
